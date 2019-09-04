8891e4c34a5fc12cd9179a57dfdf54c4df636bcf
Farrid Kuntoro <farridkuntoro@email.unikom.ac.id>
2019-09-05 00:21:00 +0700

    Perubahan Icon

--- application/views/templates/auth_header.php
+++ application/views/templates/auth_header.php
@@ -10,7 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
     <!-- App favicon -->
-    <link rel="shortcut icon" href="<?= base_url('assets/'); ?>images/fav.ico">
+    <link rel="shortcut icon" href="<?= base_url('assets/'); ?>images/favicon.ico">
 
     <!-- App css -->
     <link href="<?= base_url('assets/'); ?>css/bootstrap.min.css" rel="stylesheet" type="text/css" />
